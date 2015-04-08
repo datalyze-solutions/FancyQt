1a9932594fed423e57863233ac828f103b532a69
kaotika <m.ludwig@datalyze-solutions.com>
2015-04-08 11:24:29 +0200

    qgis test app seperated
    * qgis test app seperated
    * osm dataset added
    * changes to import

--- examples/TestAppQgis.py
+++ examples/TestAppQgis.py
@@ -1,4 +1,5 @@
 import sys
+import os
 
 import sip
 sip.setapi('QString', 2)
@@ -12,6 +13,10 @@ from PyQt4.QtCore import Qt
 import fancyqt.firefox
 
 from dummy import Ui_DummyWidget
+from qgis.core import QgsApplication
+import qgisspaf as spaf
+QgsApplication.setPrefixPath(spaf.utils.paths.defaultQgsPrefixPath(), True)
+QgsApplication.initQgis()
 
 class DummyWidget(QtGui.QWidget, Ui_DummyWidget):
     
@@ -19,6 +24,14 @@ class DummyWidget(QtGui.QWidget, Ui_DummyWidget):
         super(DummyWidget, self).__init__(parent)
         self.setupUi(self)
         self._app = app
+        self.canvas = spaf.QgsSpafMapCanvas(
+            parent=self,
+            defaultExtent=spaf.utils.extents.EXTENT_BERLIN,
+            minZoomlevel=0,
+            maxZoomlevel=15,
+        )
+        self.mapFrame.resize(1024, 768)
+        self.mapLayout.addWidget(self.canvas)
         
         self._menu = QtGui.QMenu(self)
         self.toolButton_2.setMenu(self._menu)
@@ -28,6 +41,7 @@ class DummyWidget(QtGui.QWidget, Ui_DummyWidget):
         self.actionReloadStyle.triggered.connect(self._reloadStyle)
         self._reloadStyle()
         
+        self.initMapData()
         self.setModels()
         
     def _reloadStyle(self):
@@ -37,7 +51,20 @@ class DummyWidget(QtGui.QWidget, Ui_DummyWidget):
         else:
             __import__(module_name, fromlist=[module_name])
         
-        self._app.setStyleSheet(fancyqt.firefox.stylesheet.style) 
+        self._app.setStyleSheet(fancyqt.firefox.stylesheet.style)
+        
+    def initMapData(self):
+        groupLayer = spaf.QgsSpafGroupCanvasLayer(visible=True, stackOrder=1)
+        groupLayer.setName("dummy")
+        self.canvas.mapModel().addGroupLayer(groupLayer, visible=True)
+        self.canvas._uiWidget.toolButtonLayers.animateClick(10)
+
+        path = os.path.dirname(os.path.abspath(__file__))
+        path = os.path.join(path, "osm_mapnik.xml")
+        layer = spaf.QgsSpafRasterLayer(path, "OSM Mapnik")
+        basemap = self.canvas.mapModel().addBasemap(layer)
+        basemap.setVisible(True)
+        self.canvas.mapModel().addBasemap(layer)        
         
     def setModels(self):
         model = QtGui.QStandardItemModel()
--- fancyqt/__init__.py
+++ fancyqt/__init__.py
@@ -1,6 +1,3 @@
 # -*- coding: utf-8 -*-
 __import__('pkg_resources').declare_namespace(__name__)
 __version__ = "1.0.0"
-
-import fancyqt.icons_rc
-import firefox
